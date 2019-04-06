75db02f541aae98b5b83edc851be85e5660d8436
shaunmcm <shaunmunoz145@gmail.com>
2019-04-06 02:05:46 -0700

    Update config.js

--- sleekLoad2/js/config.js
+++ sleekLoad2/js/config.js
@@ -72,7 +72,7 @@ var l_bgImageMapBased = false;
 // (Place the files in the backgrounds/images folder)
 // (You can also use external URL)
 var l_bgImages = [
-    "hero",
+    "hero.jpg",
 	"hero1.jpg",
 	"hero2.jpg",
 	"hero3.jpg",
